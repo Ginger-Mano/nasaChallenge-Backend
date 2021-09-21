f09f1a52672b883923699f4ce850d7bfe23fce64
Manon Sainton <manonsainton.dev@gmail.com>
2021-09-21 12:48:46 -0400

    add show

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,4 +4,26 @@ class LikesController < ApplicationController
         like = Like.all
         render json: like
     end
+
+    def show
+        like = Like.find(params[:id])
+        render json: like
+    end
+
+    def create
+        like = Like.create(like_params)
+        render json: like
+    end
+
+    def destroy
+        like = Like.find(params[:id])
+        like.destroy
+        render json: like
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:id, :like_count)
+    end
 end
